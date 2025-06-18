a4fd6cae4249218295b8834939c884140de64e8e
MiicheYang <yangzeyuan2020@gmail.com>
2025-06-17 18:05:46 -0700

    code

--- src/task.py
+++ src/task.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-def vsp_preprocess_function(sample):
+def single_input_image_preprocess_function(sample):
     # Load images
     image = Image.open(sample["image_input"]).convert("RGB") 
     image_output = Image.open(sample["image_output"]).convert("RGB")
@@ -25,7 +25,35 @@ def vsp_preprocess_function(sample):
 
     return conversations
 
+def multiple_input_images_preprocess_function(sample):
+
+    # Multiple input images
+    user_content = []
+    for image in sample['image_input']:
+        user_content.append({"type": "image", "image": Image.open(image).convert("RGB") })
+    user_content.append({"type": "text", "text": sample["text_input"]})
+
+    image_output = Image.open(sample["image_output"]).convert("RGB")
+
+    conversations = [
+        {
+            "role": "user", 
+            "content": user_content
+        }, 
+        {
+            "role": "assistant", 
+            "content": [
+                {"type": "image", "image": image_output}, 
+                {"type": "text", "text": sample["text_output"]}
+                ],
+        },
+    ]
+
+    return conversations
+
 task_preporcess_config = {
-    'vsp-spatial-reasoning': vsp_preprocess_function,
-    'vsp-spatial-planning': vsp_preprocess_function,
+    'vsp-spatial-reasoning': single_input_image_preprocess_function,
+    'vsp-spatial-planning': single_input_image_preprocess_function,
+    'blink-jigsaw': multiple_input_images_preprocess_function,
+    'sat': multiple_input_images_preprocess_function,
 }
